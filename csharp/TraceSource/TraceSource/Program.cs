5a05564910480116dc4a605cdc9ebe43924a0d0a
Davenson Lombard <lombard.davenson@gmail.com>
2018-08-16 17:15:02 -0400

    MongoTraceSource

---
+++ csharp/TraceSource/TraceSource/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Diagnostics;
+using MongoDB.Driver;
+using MongoDB.Driver.Core.Configuration;
+
+namespace MongoTraceSource
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Configure TraceSource
+
+            TraceSource traceSource = new TraceSource("mongodb-test", SourceLevels.Verbose);
+            var listener = new TextWriterTraceListener(Console.Out)
+            {
+                TraceOutputOptions = TraceOptions.DateTime
+            };
+            traceSource.Listeners.Add(listener);
+
+
+
+            var username = ConfigurationManager.AppSettings.Get("username");
+            var pwd = ConfigurationManager.AppSettings.Get("password");
+
+
+            //Servers List
+            List<MongoServerAddress> _servers = new List<MongoServerAddress>();
+
+            _servers.Add(new MongoServerAddress(ConfigurationManager.AppSettings.Get("server0")));
+            _servers.Add(new MongoServerAddress(ConfigurationManager.AppSettings.Get("server1")));
+            _servers.Add(new MongoServerAddress(ConfigurationManager.AppSettings.Get("server2")));
+
+
+
+            var settings = new MongoClientSettings
+            {
+
+
+                Credential = MongoCredential.CreateCredential("admin", username, pwd),
+                ReplicaSetName = ConfigurationManager.AppSettings.Get("replicaSetName"),
+                Servers = _servers,
+                UseSsl = true,
+                VerifySslCertificate = false,
+                ClusterConfigurator = _cb => {
+
+                    _cb.TraceWith(traceSource);
+                }
+
+            };
+            var client = new MongoClient(settings);
+
+
+        }
+    }
+}
---
+++ csharp/TraceSource/TraceSource/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+<<<<<<< HEAD
+[assembly: AssemblyTitle("MongoTraceSource")]
+=======
+[assembly: AssemblyTitle("TraceSource")]
+>>>>>>> 3502a6b44578c85993afb43d7d50066a5463f002
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("${AuthorCopyright}")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("1.0.*")]
+
+// The following attributes are used to specify the signing key for the assembly, 
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
